30effdc1426bba5870fc8b7416cea94c2d890cfc
Joel Järlesäter <joeljarlesater@Joels-MacBook-Pro.local>
2021-01-28 12:25:26 +0100

    lab1

---
+++ lab1/inventory.js
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>301 Moved Permanently</title>
+</head><body>
+<h1>Moved Permanently</h1>
+<p>The document has moved <a href="https://fileadmin.cs.lth.se/cs/Education/EDAF90/labs/lab1/inventory.js">here</a>.</p>
+<hr>
+<address>Apache/2.4.41 (Ubuntu) Server at fileadmin.cs.lth.se Port 80</address>
+</body></html>
